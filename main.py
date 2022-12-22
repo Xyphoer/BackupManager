d92d9de38520e81637563021f520c2e096d5576b
Xyphoer <pizzacrime3000@gmail.com>
2022-12-22 11:38:31 -0600

    begin work on ignore function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from copy import copy
-from pathlib import Path
+from time import perf_counter
 # compare modified dates to see if an already backed up file needs to be overwritten
 # https://docs.python.org/3/library/shutil.html
 # https://docs.python.org/3/library/shutil.html?#shutil.copytree
@@ -12,9 +11,31 @@ import pathlib
 
 # shutil.copy2(original, duplicate)
 
-original_tree = pathlib.PurePath("D:/coding projects/Projects/BackupManager/temp/[Cleo] Citrus")
-duplicate_tree = pathlib.PurePath("D:/coding projects/Projects/BackupManager/temp/test")
+original_tree = ""
+duplicate_tree = ""
 
-shutil.copytree(original_tree, duplicate_tree)
+original_path = pathlib.PurePath(original_tree)
+duplicate_path = pathlib.PurePath(duplicate_tree)
+
+def ignore(visiting, contents):
+    exclude = []
+
+    for item in contents:
+        file = pathlib.Path(item)
+        if file.is_file():
+            extension = visiting.replace(original_tree, "")
+            dupe_file = pathlib.Path(duplicate_path/extension/file)
+            if dupe_file.exists() and dupe_file.stat().st_mtime
+    print(visiting)
+    print(contents)
+    return exclude
+
+start = perf_counter()
+
+shutil.copytree(original_path, duplicate_path, ignore=ignore, dirs_exist_ok=True)
 # test too see if actually overwrites same name stuff if data different, and if data the same. Then check to see if creating an ignore funct is necissary.
 # test with over 260 character paths copying to external drive (maybe create a thing to detect if it's necissary for those ones and notify to transfer by self)
+
+end = perf_counter()
+
+print(end-start)
