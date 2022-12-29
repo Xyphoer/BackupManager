ba10c1bed3e954e3eeeb5b57367b43ff644e87fc
Xyphoer <pizzacrime3000@gmail.com>
2022-12-29 00:57:24 -0600

    implement light verbosity

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from time import perf_counter
-# compare modified dates to see if an already backed up file needs to be overwritten
-# https://docs.python.org/3/library/shutil.html
-# https://docs.python.org/3/library/shutil.html?#shutil.copytree
 
 import shutil
 import pathlib
 
-original_tree = "D:\\coding projects\\Projects\\BackupManager\\temp\\[Cleo] Citrus"
-duplicate_tree = "D:\\coding projects\\Projects\\BackupManager\\temp\\test"
+original_tree = ""
+duplicate_tree = ""
 
 original_path = pathlib.PurePath(original_tree)
 duplicate_path = pathlib.PurePath(duplicate_tree)
+
+copied_amount = 0
 
 #####
 # Name: ignore
@@ -20,17 +19,24 @@ duplicate_path = pathlib.PurePath(duplicate_tree)
 #              Called for the original directory and all subdirectories in original_tree when used in shutil.copytree.
 #####
 def ignore(visiting, contents):
+    global copied_amount
+
     exclude = []    # to contain unchanged previously backed up files
+    files = 0   # to contain how many files have been backed up
 
     for item in contents:
         file = pathlib.Path(visiting+'/'+item)  # use current folder's path and current item in folder to get desired file's path
         if file.is_file():
+            files += 1
             extension = visiting.replace(original_tree, "") # get the extra dirs walked from original_tree
             dupe_file = pathlib.Path(duplicate_tree + extension + "\\" + item)  # find location to check/copy duplicate file
             if dupe_file.exists() and dupe_file.stat().st_mtime == file.stat().st_mtime:
                 exclude.append(item)    # add file to be ignored if it already exists in backup and modified time of orig = dupe
+                files -= 1  # decriment files if not copying
     print("Visiting: " + visiting)
     print("Number of files: " + str(len(exclude)))
+    copied_amount += files
+    print(f"Copied: {files} Total: {copied_amount}")
     return exclude
 
 start = perf_counter()
