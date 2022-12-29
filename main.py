796911f44583465fcd2c445502548a386de8d0f7
Xyphoer <pizzacrime3000@gmail.com>
2022-12-29 00:14:47 -0600

    Clean up small details

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ from time import perf_counter
 import shutil
 import pathlib
 
-# original = pathlib.PurePath("D:/coding projects/Projects/BackupManager/temp/[Cleo]Citrus_-_01_(10bit_BD1080p_x265).mkv")
-# duplicate = pathlib.PurePath("D:/coding projects/Projects/BackupManager/temp/test.mkv")
-
-# shutil.copy2(original, duplicate)
-
 original_tree = "D:\\coding projects\\Projects\\BackupManager\\temp\\[Cleo] Citrus"
 duplicate_tree = "D:\\coding projects\\Projects\\BackupManager\\temp\\test"
 
@@ -27,12 +22,14 @@ def ignore(visiting, contents):
             dupe_file = pathlib.Path(duplicate_tree + extension + "\\" + item)
             if dupe_file.exists() and dupe_file.stat().st_mtime == file.stat().st_mtime:
                 exclude.append(item)
+    print("Visiting: " + visiting)
+    print("Number of files: " + str(len(exclude)))
     return exclude
 
 start = perf_counter()
 
 shutil.copytree(original_path, duplicate_path, ignore=ignore, dirs_exist_ok=True)
-# test too see if actually overwrites same name stuff if data different, and if data the same. Then check to see if creating an ignore funct is necissary.
+# make feeback for if something exists in backup but not in original. Implement verbosity levels.
 # test with over 260 character paths copying to external drive (maybe create a thing to detect if it's necissary for those ones and notify to transfer by self)
 
 end = perf_counter()
