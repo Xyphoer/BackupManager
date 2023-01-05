da7d76dd30a8f4e398adf7656ea9b647cfc166e7
Xyphoer <pizzacrime3000@gmail.com>
2023-01-04 18:12:03 -0600

    implement check_backup option

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ parser.add_argument("-i", "--input", required=True,
                     help="The original directory path.")
 parser.add_argument("-o", "--output", required=True,
                     help="The backup directory path.")
+parser.add_argument("-cb", "--check-backup", action="store_true",
+                    help="Checks the backup directory for any files not present in the original directory.")
 
 args = parser.parse_args()
 
-original_tree = args.input
-duplicate_tree = args.output
-
-original_path = pathlib.Path(original_tree)
-duplicate_path = pathlib.Path(duplicate_tree)
+original_path = pathlib.Path(args.input)
+duplicate_path = pathlib.Path(args.output)
 
 # check if provided path's exist
 original_exists = original_path.exists()
@@ -32,7 +31,7 @@ copied_amount = 0
 # Inputs: visiting (string) [current directory], contents (list) [contained directories & files]
 # Output: exclude (list) [files to ignore]
 # Description: ignore function for shutil.copytree - determines whether files in a directory need to be backed up.
-#              Called for the original directory and all subdirectories in original_tree when used in shutil.copytree.
+#              Called for the original directory and all subdirectories in original_path when used in shutil.copytree.
 #####
 def ignore(visiting, contents):
     global copied_amount
@@ -40,12 +39,13 @@ def ignore(visiting, contents):
     exclude = []    # to contain unchanged previously backed up files
     files = 0   # to contain how many files have been backed up
 
+    extension = visiting.replace(original_path.as_posix(), "") # get the extra dirs walked from original_path
+
     for item in contents:
         file = pathlib.Path(visiting+'/'+item)  # use current folder's path and current item in folder to get desired file's path
         if file.is_file():
             files += 1
-            extension = visiting.replace(original_tree, "") # get the extra dirs walked from original_tree
-            dupe_file = pathlib.Path(duplicate_tree + extension + "\\" + item)  # find location to check/copy duplicate file
+            dupe_file = pathlib.Path(duplicate_path.as_posix() + extension + "\\" + item)  # find location to check/copy duplicate file
             if dupe_file.exists() and dupe_file.stat().st_mtime == file.stat().st_mtime:
                 exclude.append(item)    # add file to be ignored if it already exists in backup and modified time of orig = dupe
                 files -= 1  # decriment files if not copying
@@ -55,20 +55,41 @@ def ignore(visiting, contents):
     print(f"Copying: {files} Total: {copied_amount}")
     return exclude
 
+#####
+# Name: check_backup
+# Inputs: tree (string) [directory in backup directory]
+# Output: None
+# Description: Check the backup directory and all contained directories for files not present in the original directory.
+#####
+def check_backup(tree = duplicate_path.as_posix()):
+    dupe_dir = pathlib.Path(tree)
+    extension = tree.replace(duplicate_path.as_posix(), "") # get the extra dirs walked from duplicate_path
+    for child in dupe_dir.iterdir():    # iterate over all contents of the directory
+        # if the child is a file check for it's existince in the original directory
+        if child.is_file():
+            original_file = pathlib.Path(original_path.as_posix() + extension + "\\" + child.name)
+            if not original_file.exists():
+                print(f"{child} in backup has no corresponding file at {original_file.as_posix()}")
+        # if the child is a directory, recursively call self function with that directory as the working tree
+        elif child.is_dir():
+            check_backup(tree = child.as_posix())
+    return
+
 # perform backup or output error messages depending on whether provided path's exist
 if original_exists and duplicate_exists:
     start = perf_counter()
 
     shutil.copytree(original_path, duplicate_path, ignore=ignore, dirs_exist_ok=True)
-    # make feeback for if something exists in backup but not in original. Implement verbosity levels.
-    # test with over 260 character paths copying to external drive (maybe create a thing to detect if it's necissary for those ones and notify to transfer by self)
 
     end = perf_counter()
 
     print(f"Backup finished in {round(end-start, ndigits=2)} seconds")    # output time taken for backup
+
+    if args.check_backup:
+        check_backup()
 elif not original_exists and duplicate_exists:
-    print(f"Input path {original_tree} doesn't exist.")
+    print(f"Input path {original_path.as_posix()} doesn't exist.")
 elif not duplicate_exists and original_exists:
-    print(f"Output path {duplicate_path} doesn't exist.")
+    print(f"Output path {duplicate_path.as_posix()} doesn't exist.")
 else:
-    print(f"Input path {original_tree} and output path {duplicate_path} don't exist.")
+    print(f"Input path {original_path.as_posix()} and output path {duplicate_path.as_posix()} don't exist.")
