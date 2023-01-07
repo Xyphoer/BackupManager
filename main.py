bf3849413dc13456e2551d08923bedea6ff8dada
Xyphoer <pizzacrime3000@gmail.com>
2023-01-07 02:10:38 -0600

    implement skipping of folders and files

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import argparse
 parser = argparse.ArgumentParser(prog="Backup Manager",
                                  description="Manages backup of everything contained in a specified directory")
 parser.add_argument("-i", "--input", required=True,
-                    help="The original directory path.")
+                    help="The original directory path. If ommitted will not backup anything.")
 parser.add_argument("-o", "--output", required=True,
-                    help="The backup directory path.")
+                    help="The backup directory path. If ommitted will not backup anything.")
+parser.add_argument("-eb", "--exclude-backup", nargs="*",
+                    help="Excludes specified directories/files from backup (todo). If used without arguments, doesn't backup any files.")
 parser.add_argument("-cb", "--check-backup", action="store_true",
                     help="Checks the backup directory for any files not present in the original directory.")
 
@@ -26,6 +28,9 @@ duplicate_exists = duplicate_path.exists()
 # keep track of how many files are going to be copied
 copied_amount = 0
 
+# store files and directories to skip in backup process
+skip = args.exclude_backup if args.exclude_backup != None else []
+
 #####
 # Name: ignore
 # Inputs: visiting (string) [current directory], contents (list) [contained directories & files]
@@ -35,24 +40,42 @@ copied_amount = 0
 #####
 def ignore(visiting, contents):
     global copied_amount
+    global skip
 
     exclude = []    # to contain unchanged previously backed up files
-    files = 0   # to contain how many files have been backed up
+    folder_skip = []    # to contain any files to skip in this folder
+    copy = 0   # to contain how many files have been backed up
+    files = 0   # to contain how many files are contained
 
-    extension = visiting.replace(str(original_path), "") # get the extra dirs walked from original_path
-
-    for item in contents:
-        file = Path(visiting+'/'+item)  # use current folder's path and current item in folder to get desired file's path
-        if file.is_file():
-            files += 1
-            dupe_file = Path(str(duplicate_path) + extension + "\\" + item)  # find location to check/copy duplicate file
-            if dupe_file.exists() and dupe_file.stat().st_mtime == file.stat().st_mtime:
-                exclude.append(item)    # add file to be ignored if it already exists in backup and modified time of orig = dupe
-                files -= 1  # decriment files if not copying
     print("Visiting: " + visiting)
-    print("Number of files already backed up: " + str(len(exclude)))
-    copied_amount += files
-    print(f"Copying: {files} Total: {copied_amount}")
+
+    if visiting in skip:
+        print("Skipping folder.") # test is skipping a parent dir skips child dirs
+        exclude = contents
+    else:
+        extension = visiting.replace(str(original_path), "") # get the extra dirs walked from original_path
+
+        for item in contents:
+            file = Path(visiting+'/'+item)  # use current folder's path and current item in folder to get desired file's path
+            if file.is_file():
+                if str(file) in skip:
+                    print("Skipping: " + item)
+                    folder_skip.append(item)
+                else:
+                    files += 1
+                    copy += 1
+
+                    dupe_file = Path(str(duplicate_path) + extension + "\\" + item)  # find location to check/copy duplicate file
+                    if dupe_file.exists() and dupe_file.stat().st_mtime == file.stat().st_mtime:
+                        exclude.append(item)    # add file to be ignored if it already exists in backup and modified time of orig = dupe
+                        copy -= 1  # decriment copy if not copying
+        
+        print(f"Number of {'unskipped ' if folder_skip != [] else ''}files already backed up: {str(len(exclude))}/{str(files)}")
+        exclude += folder_skip
+    
+        copied_amount += copy
+        print(f"Copying: {copy} Total: {copied_amount}")
+    
     return exclude
 
 #####
@@ -76,14 +99,19 @@ def check_backup(tree = str(duplicate_path)):
     return
 
 # perform backup or output error messages depending on whether provided path's exist
-if original_exists and duplicate_exists:
-    start = perf_counter()
+if (original_exists and duplicate_exists):
 
-    copytree(original_path, duplicate_path, ignore=ignore, dirs_exist_ok=True)
+    # if exclude_backup is specified without arguments, don't perform backup operation
+    if args.exclude_backup == []:
+        print("Skipping Backup.")
+    else:
+        start = perf_counter()
 
-    end = perf_counter()
+        copytree(original_path, duplicate_path, ignore=ignore, dirs_exist_ok=True)
 
-    print(f"Backup finished in {round(end-start, ndigits=2)} seconds")    # output time taken for backup
+        end = perf_counter()
+
+        print(f"Backup finished in {round(end-start, ndigits=2)} seconds")    # output time taken for backup
 
     if args.check_backup:
         check_backup()
